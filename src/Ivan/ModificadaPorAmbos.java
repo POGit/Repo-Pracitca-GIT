3a830aadce90dbde38d5ccb439acebd45513edad
Vespertino <Vespertino@C7PC22>
2015-04-29 16:37:50 +0200

    Commit de ivan

---
+++ src/Ivan/ModificadaPorAmbos.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Ivan;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class ModificadaPorAmbos {
+    public void Ivan(){
+        System.out.print("Ivan te saluda");
+    }
+}
